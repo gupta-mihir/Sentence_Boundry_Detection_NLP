eca91a981bffbe834744575f38b0c514221376ff
Mihir Gupta <69476837+gupta-mihir@users.noreply.github.com>
2022-09-26 23:14:20 -0400

    added scikit learn library for future decisiontree

--- SBD.py
+++ SBD.py
@@ -1,4 +1,12 @@
-print ("Hello World!")
+import sklearn
+import pandas as pd
+#Learning how to use sklearn 
+from sklearn.datasets import load_iris
+iriss = load_iris()
+df_iris = pd.DataFrame(iriss.data, columns=iriss.feature_names)
+df_iris.head()
+
 with open('SBD.train.txt') as f:
     lines = f.read(1000)
     print(lines, end='')
+    #need to import learn library
