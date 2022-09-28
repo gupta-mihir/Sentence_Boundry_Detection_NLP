6e3914d8bf8c0c724b5780152ee9c7fde16ed69f
Mihir Gupta <69476837+gupta-mihir@users.noreply.github.com>
2022-09-28 10:33:24 -0400

    count of EOS and NEOS found out

--- SBD.py
+++ SBD.py
@@ -8,6 +8,19 @@ with open('SBD.train.txt') as f:
     import re
     myex = re.compile(r"(TOK)")
     final_str = re.sub(myex, '',s1)
-    print(final_str, end='')
+    init_counter = 0
+    neos_counter = 0
+    split_str = final_str.splitlines()
+    for x in split_str:
+        if '.' in x and ' EOS' in x:
+            init_counter += 1
+        if '.' in x and 'NEOS' in x:
+            neos_counter += 1
+   # print(final_str, end='')
+    #print(split_str)
+    print ('\n')
+    print('Number of EOS:', init_counter - neos_counter)
+    print('Number of NEOS:', neos_counter)
+
     #print(s1, end='')
     
